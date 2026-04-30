aba87391263a3bea05e2bb057a104caf1e9f48e3
julia2524 <61957239+julia2524@users.noreply.github.com>
2026-04-30 13:21:04 +0900

    making lists  with tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full max-w-screen-sm p-5 shadow-lg rounded-2xl flex flex-col gap-2 md:flex-row md:items-start">
-        <div className="flex flex-col w-full ">
-          <input
-            className="w-full rounded-full h-10 outline-none bg-gray-200 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow invalid:focus:ring-red-500 peer"
-            type="email"
-            required
-            placeholder="Email address here..."
-          />
-          <span className="text-red-500 font-medium hidden peer-invalid:block mt-1">
-            Email is required.
-          </span>
-        </div>
-        <button className="bg-black text-white py-2 outline-none rounded-full active:scale-90 transition-transform font-medium  md:px-8 ">
-          Search
-        </button>
+      <div className="bg-white w-full max-w-screen-sm p-5 shadow-lg rounded-2xl flex flex-col gap-3 ">
+        {["Jane Cooler", "Leslie Alexander", "Eleanor Pena", "Nicolas"].map(
+          (person, index) => (
+            <div key={index} className="flex justify-between items-center">
+              <div className="flex gap-3 items-center">
+                <div className="size-7 bg-red-200 rounded-full" />
+                <span>{person}</span>
+                <div className="size-5 bg-red-500 rounded-full flex items-center justify-center">
+                  <span className="text-white text-xs font-bold text-center">
+                    {index}
+                  </span>
+                </div>
+              </div>
+              <div className="size-7 bg-gray-100 rounded-full flex items-center justify-center">
+                <span className=" text-xs text-slate-900 text-center">❯</span>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </main>
   );
