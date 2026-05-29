5d6efe9e60de01ffb10df0308a510c0c620e542a
julia2524 <61957239+julia2524@users.noreply.github.com>
2026-05-29 22:44:15 +0900

    add button editing

--- components/product-form.tsx
+++ components/product-form.tsx
@@ -36,24 +36,34 @@ export default function ProductForm({
     },
   });
 
-  const onValid = async (data: ProductType) => {
-    if (!file) {
+  // const onValid = async (data: ProductType) => {
+  //   if (!file) {
+  //     alert("사진을 추가해주세요!");
+  //     return;
+  //   }
+  //   const formData = new FormData();
+  //   formData.append("title", data.title);
+  //   formData.append("price", data.price.toString());
+  //   formData.append("description", data.description);
+  //   formData.append("photo", file);
+
+  //   return uploadProduct(formData);
+  // };
+  const action = async (formData: FormData) => {
+    const file = formData.get("photo");
+    if (!file || (file as File).size === 0) {
       alert("사진을 추가해주세요!");
       return;
     }
-    const formData = new FormData();
-    formData.append("title", data.title);
-    formData.append("price", data.price.toString());
-    formData.append("description", data.description);
-    formData.append("photo", file);
-
-    return uploadProduct(formData);
+    console.log(formData.get("title"));
+    await uploadProduct(formData);
   };
   return (
-    <form
-      onSubmit={handleSubmit(onValid, (errors) => console.log(errors))}
-      className="p-5 flex flex-col gap-5"
-    >
+    // <form
+    //   onSubmit={handleSubmit(onValid, (errors) => console.log(errors))}
+    //   className="p-5 flex flex-col gap-5"
+    // >
+    <form action={action} className="p-5 flex flex-col gap-5">
       <ProductImageForm
         preview={preview}
         setPreview={setPreview}
